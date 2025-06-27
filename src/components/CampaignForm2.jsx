4f233c818acf059da3e803d99d38e362bfef0ff4
Aditya Prasad <adityak2541@gmail.com>
2025-06-27 16:11:36 +0530

    Update CampaignForm2.jsx

--- src/components/CampaignForm2.jsx
+++ src/components/CampaignForm2.jsx
@@ -30,7 +30,7 @@ const CampaignForm2 = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/submit", {
+      const res = await fetch("https://gwm-campaign-backend.onrender.com/api/submit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...formData, campaignId: campaign.id })
