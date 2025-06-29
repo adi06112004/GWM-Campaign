92578b2350a1386b20a9718a19dab467bcde6668
Aditya Prasad <adityak2541@gmail.com>
2025-06-29 18:50:56 +0530

    Update CampaignForm4.jsx

--- src/components/CampaignForm4.jsx
+++ src/components/CampaignForm4.jsx
@@ -35,7 +35,7 @@ const CampaignForm4 = () => {
       const data = await res.json();
       await new Promise(resolve => setTimeout(resolve, 5000));
       if (res.ok || data.alreadyExists) {
-        window.open(campaign.redirectUrl, "_blank");
+        window.open(campaign.redirectUrl);
       } else {
         alert(data.error || "Submission failed");
       }
