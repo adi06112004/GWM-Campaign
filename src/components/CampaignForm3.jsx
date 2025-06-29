5c5da510ef39f5559ff927b251a16f8a5d5262f7
Aditya Prasad <adityak2541@gmail.com>
2025-06-29 15:18:13 +0530

    Update CampaignForm3.jsx

--- src/components/CampaignForm3.jsx
+++ src/components/CampaignForm3.jsx
@@ -40,7 +40,7 @@ const CampaignForm3 = () => {
       });
       const data = await res.json();
 
-      await new Promise((resolve) => setTimeout(resolve, 12000));
+      await new Promise((resolve) => setTimeout(resolve, 5000));
 
       if (res.ok || data.alreadyExists) {
         window.open(campaign.redirectUrl, "_blank");
