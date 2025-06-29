0e4f378ce1819c9240da652685a57e7d2a012d46
Aditya Prasad <adityak2541@gmail.com>
2025-06-29 19:06:51 +0530

    Update CampaignForm5.jsx

--- src/components/CampaignForm5.jsx
+++ src/components/CampaignForm5.jsx
@@ -48,7 +48,7 @@ const CampaignForm5 = () => {
       await new Promise((resolve) => setTimeout(resolve, 5000));
 
       if (res.ok || data.alreadyExists) {
-        window.open(campaign.redirectUrl);
+      window.location.href = campaign.redirectUrl;
       } else {
         alert(data.error || "Submission failed");
       }
