ebfce448906d2780ce214f7c6fd5bb8a0e574455
Aditya Prasad <adityak2541@gmail.com>
2025-11-12 21:01:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         {/* <Route path="/campaign2" element={<CampaignForm2 />} /> */}
         {/* <Route path="/campaign3" element={<CampaignForm3 />} /> */}
         <Route path="/campaign4" element={<CampaignForm4 />} />
-        {/* <Route path="/campaign5" element={<CampaignForm5 />} /> */}
+        <Route path="/campaign5" element={<CampaignForm5 />} />
         {/* <Route path="/campaign6" element={<CampaignForm6/>} /> */}
         <Route path="/campaign7" element={<CampaignForm7/>} />
         <Route path="/lead/campaign1" element={<LeadList campaignId="campaign1"/>} />
